96fb866ad97a281fee917eec4a9e23519ddaf69e
Pooyan <mail>
2026-04-02 15:03:05 -0400

    automative climb

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
  * This program is configured to control a 2020 Everybot robot.
@@ -15,82 +16,91 @@ import edu.wpi.first.util.sendable.SendableRegistry;
  * mode is enabled, don't change their names.
  */
 public class Robot extends TimedRobot {
-  //Controller
-  XboxController controller = new XboxController(0);
+    // Controller
+    XboxController controller = new XboxController(0);
 
-  /*  Drive Train - Create 2 motor controllers, one for each side
-  *   and a DifferentialDrive which lets us easily control the drive train
-  */
-  VictorSP leftMotor = new VictorSP(0);
-  VictorSP rightMotor = new VictorSP(1);
-  DifferentialDrive drive = new DifferentialDrive(leftMotor, rightMotor);
+    /* Drive Train - Create 2 motor controllers, one for each side
+     * and a DifferentialDrive which lets us easily control the drive train
+     */
+    VictorSP leftMotor = new VictorSP(0);
+    VictorSP rightMotor = new VictorSP(1);
+    DifferentialDrive drive = new DifferentialDrive(leftMotor, rightMotor);
 
-  //Intake - Create a motor controller and a pneumatic solenoid
-  VictorSP intakeMotor = new VictorSP(3);
-  Solenoid intakePneumatic = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
-  
-  //Climb motor
-  VictorSP climbMotor = new VictorSP(4);
+    // Intake - Create a motor controller and a pneumatic solenoid
+    VictorSP intakeMotor = new VictorSP(3);
+    Solenoid intakePneumatic = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
 
-    /**
-   * This method is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    //These methods give names to our shuffleboard livewindow outputs
-    SendableRegistry.add(drive, "drive");
-    SendableRegistry.add(intakeMotor, "intakeMotor");
-    SendableRegistry.add(intakePneumatic, "intakePneumatic");
-    // Add climb to the shuffleboard
-    SendableRegistry.add(climbMotor, "climb");
-  }
+    // Climb motor
+    VictorSP climbMotor = new VictorSP(4);
 
-  /**
-   * This method is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-    //arcade drive has two parmeters, one controller axis for forward and reverse
-    //and one for turning the robot left and right
-    drive.arcadeDrive(-1 * controller.getLeftY(), controller.getRightX());
+    // Climb limit switches
+    DigitalInput climbInput = new DigitalInput(0);   // bottom sensor
+    DigitalInput climbInput2 = new DigitalInput(1);  // top sensor
 
-    //This is a simplified intake control methods
-    //If the left bumper button on the controller is pressed 
-    //the pneumatic deploys the intake, when it isn't pressed the intake is up
-    if (controller.getLeftBumper()){
-      intakePneumatic.set(true);
-    } else {
-      intakePneumatic.set(false);
+    @Override
+    public void robotInit() {
+        // Give names to shuffleboard/livewindow outputs
+        SendableRegistry.add(drive, "drive");
+        SendableRegistry.add(intakeMotor, "intakeMotor");
+        SendableRegistry.add(intakePneumatic, "intakePneumatic");
+        SendableRegistry.add(climbMotor, "climb");
     }
 
-    //If the right bumper button on the controller is pressed the intake motor is spinning forward (in)
-    //If the right trigger axis is pressed past 0.5 the intake motor is set to reverse (out)
-    //If niether is pressed the motor stops
-    if(controller.getRightBumper()){
-      intakeMotor.set(1.0);
-    } else if (controller.getRightTriggerAxis() > 0.5){
-      intakeMotor.set(-1.0);
-    } else{
-      intakeMotor.stopMotor();
-    }
+    @Override
+    public void teleopPeriodic() {
+        // Drive control: arcade drive
+        drive.arcadeDrive(-1 * controller.getLeftY(), controller.getRightX());
 
-    //This will be the climb for teleop which will run in a loop every 0.2 seconds
-    if(controller.getAButtonPressed() && controller.getYButtonPressed()){
-      climbMotor.set(0);
-    }
-    else{
-      if(controller.getYButtonPressed()){
-        climbMotor.set(1);
-      }
-      else if(controller.getYButtonPressed()){
-        climbMotor.set(-1);
-      }
-      else{
-        climbMotor.set(0);
-      }
+        // Intake pneumatic control
+        if (controller.getLeftBumper()) {
+            intakePneumatic.set(true);
+        } else {
+            intakePneumatic.set(false);
+        }
 
+        // Intake motor control
+        if (controller.getRightBumper()) {
+            intakeMotor.set(1.0);
+        } else if (controller.getRightTriggerAxis() > 0.5) {
+            intakeMotor.set(-1.0);
+        } else {
+            intakeMotor.stopMotor();
+        }
+
+        // ---------------- Climb logic ----------------
+        // Read sensors
+        boolean bottomLimit = climbInput.get();   // true if bottom sensor is triggered
+        boolean topLimit = climbInput2.get();     // true if top sensor is triggered
+
+        if (controller.getAButton()) {
+            // Automatic climb while holding A button
+            // Motor moves up unless top limit is reached
+            if (!topLimit) {
+                climbMotor.set(1);  // climb up
+            }
+            // Motor moves down unless bottom limit is reached
+            else if (!bottomLimit) {
+                climbMotor.set(-1); // climb down
+            }
+            // Stop motor if both limits are triggered
+            else {
+                climbMotor.set(0);
+            }
+        } else {
+            // Manual climb control
+            // X button: manual climb up
+            if (controller.getXButton()) {
+                    climbMotor.set(1);
+            }
+            // Y button: manual climb down
+            else if (controller.getYButton()) {
+                    climbMotor.set(-1);
+            } 
+            // No climb buttons pressed
+            else {
+                climbMotor.set(0);
+            }
+        }
+        // ------------------------------------------------
     }
-  }
-    
 }
